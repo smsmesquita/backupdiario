ba333e311d6735609f0bf403650f493178379170
Samuel Lopes <101944394+smsmesquita@users.noreply.github.com>
2022-10-02 23:23:04 -0300

    Add files via upload

--- novocadastro.py
+++ novocadastro.py
@@ -20,15 +20,48 @@ class cadastrar():
        options.add_argument('--headless')
       # navegador = webdriver.Chrome(service= service, options= options)
        navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-      # navegador.get(f"file:///C:/Users/Usu%C3%A1rio/OneDrive/SAMUEL/PROGRAMACAO/Novo_Projeto/index.html") 
+     #  navegador.get(f"file:///C:/Users/Usu%C3%A1rio/OneDrive/SAMUEL/PROGRAMACAO/Novo_Projeto/index.html") 
+      # navegador.get(f"file:///C:/Users/Usu%C3%A1rio/Desktop/Novo_Projeto/index1.html")
        navegador.get(f"http://cnpj.info/" + str(caminho))
        time.sleep(5)
        result = navegador.find_element(By.XPATH, '/html/body').text
        novoresult =  result.splitlines()
+       
        nomedaempresa = novoresult[5]
-       print(nomedaempresa[16:])
+       fantasia = novoresult[6]
+       dataatividade = novoresult[7]
+       capital = novoresult[11]
+       opcao = novoresult[13]
+       endereco = novoresult[16]
+       complemento= novoresult[17]
+       bairro = novoresult[18]
+       cep = novoresult[19]
+       telefone = novoresult[22]
+    #   print(novoresult)
+     #  busca = [s for s in novoresult if "Qualificação" in s]
+       
+       busca1 = [x for x in novoresult if "CPF***" in x]
+       y = (len(busca1))
+       if y != 1:
+           for i in range(y):
+               a = (busca1[i])
+               b = a.split()
+               c = len(b)
+               d_ate_entrada = c-1
+               e = b[1:d_ate_entrada]
+               socio = (len(e))-1
+               print(e[socio:])
+               print(e[:socio])
+       else:
+          
+           busca1 = [x for x in novoresult if "Nome" in x]
+           y = (len(busca1))
+           print(busca1[1])
 
-
+#é necessario verificar o caso de empresarios individual que nao mostram quem é o sócio
+def tabelaclientes():
+    colunas = ['NCM', 'Descrição']
+    dados = pd.DataFrame(columns=colunas)  
 
 
 
@@ -37,7 +70,7 @@ class cadastrar():
 #def consulta_simples():
 
 if __name__ == "__main__":
-    caminho = '05975390000139'
+    caminho = '11320004000173'
     cd = cadastrar()
     cd.cadastrar_clientes(caminho)
   
